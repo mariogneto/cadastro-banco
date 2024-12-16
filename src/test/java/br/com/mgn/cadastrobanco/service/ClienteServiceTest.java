7bd3011c6db941b700e676c6b4ee3f88312749af
mariogneto <mariogneto@gmail.com>
2024-12-16 14:12:57 -0300

    testes unitarios

--- src/test/java/br/com/mgn/cadastrobanco/service/ClienteServiceTest.java
+++ src/test/java/br/com/mgn/cadastrobanco/service/ClienteServiceTest.java
@@ -1,22 +1,58 @@
 package br.com.mgn.cadastrobanco.service;
 
+import br.com.mgn.cadastrobanco.controller.dto.ClienteDTO;
+import br.com.mgn.cadastrobanco.controller.dto.EnderecoDTO;
 import br.com.mgn.cadastrobanco.entity.Cliente;
 import br.com.mgn.cadastrobanco.entity.Endereco;
+import br.com.mgn.cadastrobanco.repository.ClienteRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
-class ClienteServiceTest {
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class ClienteServiceTest {
 
-    @Test
-    void deveCadastrarClienteComSucesso() {
-        Endereco endereco = new Endereco("Rua A", "123", "Bairro X", "Cidade Y", "Estado Z", "12345-678");
-        Cliente cliente = new Cliente(UUID.randomUUID(),"12345678900", "João Silva", LocalDate.of(1990, 1, 1), "999999999", endereco);
-        //Cliente salvo = clienteService.salvar(cliente);
-        //assertNotNull(salvo);
-    }
+    @Mock
+    private ClienteRepository clienteRepository;
+
+    @InjectMocks
+    private ClienteService clienteService;
+
+//    @Test
+//    void deveCadastrarClienteComSucesso() {
+//        var salvo = clienteServiceMock.criarCliente(
+//                        new ClienteDTO("12345678900", "João Silva", LocalDate.of(1990, 1, 1), "999999999",
+//                                new EnderecoDTO("Rua A", "123", "Bairro X", "Cidade Y", "Estado Z", "12345-678")))
+//                .orElse(null);
+//        assertNotNull(salvo);
+//    }
+
+//    @Test
+//    void deveCadastrarClienteComSucesso() {
+//        var clienteDTO = new ClienteDTO("12345678900", "João Silva", LocalDate.of(1990, 1, 1), "999999999",
+//                new EnderecoDTO("Rua A", "123", "Bairro X", "Cidade Y", "Estado Z", "12345-678"));
+//
+//        var cliente = new Cliente();
+//        cliente.setCpf("12345678900");
+//        cliente.setNome("João Silva");
+//        cliente.setDataNascimento(LocalDate.of(1990, 1, 1));
+//        cliente.setTelefone("999999999");
+//        cliente.setEndereco(new Endereco("Rua A", "123", "Bairro X", "Cidade Y", "Estado Z", "12345-678"));
+//
+//        when(clienteRepository.save(cliente)).thenReturn(cliente);
+//
+//        var salvo = clienteService.criarCliente(clienteDTO).orElse(null);
+//
+//        assertNotNull(salvo);
+//    }
 
 }
