692ddfc56eb9f7c043f385dda04df87b454e1882
mariogneto <mariogneto@gmail.com>
2024-12-16 21:14:32 -0300

    ajustes

--- src/main/java/br/com/mgn/cadastrobanco/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/mgn/cadastrobanco/exception/GlobalExceptionHandler.java
@@ -4,13 +4,13 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
